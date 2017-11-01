bf57d18b60e3de89ba2973bba29b62f0439788c5
huangfengya <jingyun971547468@163.com>
2017-11-01 18:40:56 +0800

    e

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-  <m-header></m-header>
+    <m-header></m-header>
   </div>
 </template>
 
 <script>
-  import Mheader from 'components/m-header/m-header'
+  import Mheader from './components/m-header/m-header'
 
   export default {
     components: {
--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,37 +1,39 @@
 <template>
   <div class="m-header">
-      <div class="icon"></div>
-      <h1 class="text">Music</h1>
-      <router-link tag="div" class="mine" to='/user'>
-        <i class="icon-mine"></i>
-      </router-link>
+    <div class="icon"></div>
+    <h1 class="text">Music</h1>
+    <router-link tag="div" class="mine" to='/user'>
+      <i class="icon-mine"></i>
+    </router-link>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {}
 </script>
 
 <style lang="less">
-    @import "@common/less/mixin.less";
+.bg-image(@url) {
+  background-image: url("@{url}@2x.png");
+  @media (-webkit-min-device-pixel-ratio: 3), (min-device-pixel-ratio: 3) {
+    background-image: url('{url}@3x.png')
+  }
+}
 
-    .m-header{
-        position: relative;
-        height: 44px;
-        text-align: center;
-        color: rgba(255, 205, 49, 0.5);
-        font-size: 0px;
-        .icon{
-            display: inline-block;
-            vertical-align: top;
-            margin-top: 6px;
-            width: 30px;
-            height: 32px;
-            .bg-image("logo");
-            
-        }
-    }
+.m-header {
+  position: relative;
+  height: 44px;
+  text-align: center;
+  color: rgba(255, 205, 49, 0.5);
+  font-size: 0px;
+  .icon {
+    display: inline-block;
+    vertical-align: top;
+    margin-top: 6px;
+    width: 30px;
+    height: 32px;
+    .bg-image(logo);
+  }
+}
 </style>
 
