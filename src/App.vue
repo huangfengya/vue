c1a3eeeddb54b5b922592d025e1b44db42eb8aad
huangfengya <jingyun971547468@163.com>
2017-11-01 15:56:30 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  <m-header></m-header>
+  </div>
+</template>
+
+<script>
+  import Mheader from 'components/m-header/m-header'
+
+  export default {
+    components: {
+      Mheader
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="m-header">
+      <div class="icon"></div>
+      <h1 class="text">Music</h1>
+      <router-link tag="div" class="mine" to='/user'>
+        <i class="icon-mine"></i>
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="less">
+    @import "@common/less/mixin.less";
+
+    .m-header{
+        position: relative;
+        height: 44px;
+        text-align: center;
+        color: rgba(255, 205, 49, 0.5);
+        font-size: 0px;
+        .icon{
+            display: inline-block;
+            vertical-align: top;
+            margin-top: 6px;
+            width: 30px;
+            height: 32px;
+            .bg-image("logo");
+            
+        }
+    }
+</style>
+
